9450e9c93e8a2126719edffb03b93d5f5f997e2f
Pinghang Fan <pinghangfan1022@gmail.com>
2022-02-09 14:48:56 +1300

    added functionality to export summarty table as csv

--- script.js
+++ script.js
@@ -64,11 +64,60 @@ const displayOnPage = (imgList) => {
     document.getElementById("dWrapper").innerHTML += "<button onclick='downloadIndividualImages()'>Download as Seperate Files</button>"
 }
 
+const readTable = (file) => {
+    let table;
+    let start = 0;
+    let end = 0;
+
+    let index = file.indexOf("Summary Table");
+    if (index != -1) {
+        start = file.indexOf("<tr ", index);
+        end = file.indexOf("</table>", index);
+        table = file.substring(start, end);
+        document.getElementById("summary").innerHTML = table;
+        document.getElementById("tableWrapper").innerHTML += "<button onclick='downloadTableAsCSV()'>Download as CSV</button>"
+    }
+
+    
+    return table;
+}
+
+const downloadTableAsCSV = (table_id, separator = ',') => {
+    // Select rows from table_id
+    var rows = document.querySelectorAll('table#' + "summary" + ' tr');
+    // Construct csv
+    var csv = [];
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll('td, th');
+        for (var j = 0; j < cols.length; j++) {
+            // Clean innertext to remove multiple spaces and jumpline (break csv)
+            var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+            // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+            data = data.replace(/"/g, '""');
+            // Push escaped string
+            row.push('"' + data + '"');
+        }
+        csv.push(row.join(separator));
+    }
+    var csv_string = csv.join('\n');
+    // Download it
+    var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+    var link = document.createElement('a');
+    link.style.display = 'none';
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
 window.onload = () => {
     document.getElementById('inputfile').addEventListener('change', function () {
         var fr = new FileReader();
         fr.onload = function () {
             imgList = readHTMLFile(fr.result);
+            readTable(fr.result);
             displayOnPage(imgList);
         }
         fr.readAsText(this.files[0]);
